a5f90388df94247d50078412e63559b89e135167
Ahmed Mohamed <78311079+ahmedmo112@users.noreply.github.com>
2021-08-15 21:59:50 +0200

    Update dio_helper.dart

--- lib/shared/network/remote/dio_helper.dart
+++ lib/shared/network/remote/dio_helper.dart
@@ -11,7 +11,7 @@ class DioHelper {
   }
 
  static  Future<Response> getData(
-   {required String url, required Map<String , dynamic>? query}
+   {required String url, required Map<String , dynamic> query}
    ) async {
   return await  dio.get(url, queryParameters: query);
   }
